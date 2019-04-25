47810c204c5f329ffb70223d011152ce6d133c37
Roman Jędras <romanjedras@o2.pl>
2019-04-25 14:11:40 +0200

    Counter

--- js/script.js
+++ js/script.js
@@ -1,2 +1,42 @@
-const app = React.createElement(App);
-ReactDOM.render(app, document.getElementById('app'));
+const Counter = React.createClass({
+	getInitialState: function() {
+		return {
+			counter: 0
+		};
+	},
+	
+	increment: function() {
+		this.setState({
+			counter: this.state.counter + 1
+		});
+	},
+	
+	decrement: function() {
+		this.setState({
+			counter: (this.state.counter === 1) ? this.state.counter  : this.state.counter  - 1
+		});
+	},
+	
+	incrementHalv: function() {
+		this.setState({
+			counter: this.state.counter + 1.5
+		});
+	},
+	
+	
+	render: function() {
+		return React.createElement('div', {},
+			React.createElement('button', {onClick: this.decrement}, 'Licznik -'),
+			React.createElement('button', {onClick: this.increment}, 'Licznik +'),
+			React.createElement('button', {onClick: this.incrementHalv}, 'Licznik + 1.5'),
+			
+			React.createElement('span', {}, 'Licznik ' + this.state.counter)
+		);
+	}
+});
+
+
+
+
+let element = React.createElement(Counter);
+ReactDOM.render(element, document.getElementById('app'));
